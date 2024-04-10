c7c3c91c95c839177bb1992ccbfe374edef761e7
dongxin <dongxin@dongxindeMacBook-Air.local>
2024-04-10 20:40:25 +0800

    Initial Commit

---
+++ src/router/index.js
@@ -0,0 +1,40 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/booking/create',
+      name: 'booking-create',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/BookingView.vue')
+    },
+    {
+      path: '/booking/:id',
+      name: 'view-booking',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/BookingView.vue')
+  },
+  {
+    path: '/booking/:id/update',
+    name: 'update-booking',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import('../views/BookingView.vue')
+  },
+  ]
+})
+
+export default router
